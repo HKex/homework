7d819e976c56c3dcf0bc0e30ac3b3a8196548172
HKex <34960121+HKex@users.noreply.github.com>
2022-10-20 16:02:07 +0800

    Update star.cpp
    winwinwinwiwiniwn

--- star.cpp
+++ star.cpp
@@ -2,26 +2,75 @@
 using namespace std;
 int main()
 {
-    int a,c;
-    cin >>a;
-      cout<<"  *"<<endl;
-      cout<<" * *"<<endl;
-      cout<<"*   *"<<endl;
-      cout<<" * *"<<endl;
-      cout<<"  *"<<endl;     
+	int n,i;
+cin>>n;
+    if( n < 4 )
+{
+    cout<<"  *"<<endl;
+	cout<<" * *"<<endl;
+	cout<<"*   *"<<endl;
+	cout<<" * *"<<endl;
+	cout<<"  *"<<endl;
+	for(int c=0;c<(n - 1);c++)
     
-  if (a>2)
-  {
-    for(int c=0;c<(a - 1);c++)
-    {
-      cout<<" * *"<<endl;
-      cout<<"*   *"<<endl;
-      cout<< " * *"<<endl;
-      cout<<"  *"<<endl;
+      cout<<" * *"<<endl<<"*   *"<<endl<<" * *"<<endl<<"  *"<<endl; 
     }
-  }
+    else if( n > 4 && n < 6)
+	{
+	cout<<"    *"<<endl; 
+	cout<<"   * *"<<endl;
+	cout<<"  *   *"<<endl;
+	cout<<" *     *"<<endl;
+	cout<<"*       *"<<endl;
+	cout<<" *     *"<<endl;
+	cout<<"  *   *"<<endl;
+	cout<<"   * *"<<endl;
+	cout<<"    *"<<endl;
+	for(int c=0;c<(n - 1);c++)
+	cout<<"   * *"<<endl<<"  *   *"<<endl<<" *     *"<<endl<<"*       *"<<endl<<" *     *"<<endl<<"  *   *"<<endl<<"   * *"<<endl<<"    *"<<endl;
+	}
+	else if(n>5)
+{
+	cout<<"         *"<<endl; 
+	cout<<"        * *"<<endl;
+	cout<<"       *   *"<<endl;
+	cout<<"      *     *"<<endl;
+	cout<<"     *       *"<<endl;
+	cout<<"    *         *	"<<endl;
+	cout<<"   *           *	"<<endl;
+	cout<<"  *             *"<<endl;
+	cout<<" *               *	"<<endl;
+	cout<<"*                 *	"<<endl; 
+	cout<<" *               *	"<<endl;
+	cout<<"  *             *"<<endl;
+	cout<<"   *           *	"<<endl;
+	cout<<"    *         *	"<<endl;
+	cout<<"     *       *"<<endl;
+	cout<<"      *     *"<<endl;
+	cout<<"	       *   *"<<endl;
+	cout<<"        * *"<<endl;
+	cout<<"         *"<<endl;
+	
+	for(int c=0;c<(n - 1);c++)
     
-
-
-}//？
-//我日了
+      cout<<"        * *"
+	  <<endl<<"       *   *"
+	  <<endl<<"      *     *"
+	  <<endl<<"     *       *"
+	  <<endl<<"    *         *	"
+	  <<endl<<"   *           *	"
+	  <<endl<<"  *             *"
+	  <<endl<<" *               *	"
+	  <<endl
+	<<"*                 *	"<<endl
+	<<" *               *	"<<endl
+	<<"  *             *"<<endl
+	<<"   *           *	"<<endl
+	<<"    *         *	"<<endl
+	<<"     *       *"<<endl
+	<<"      *     *"<<endl
+	<<"	       *   *"<<endl
+<<"        * *"<<endl
+	<<"         *"<<endl;
+	} 
+} 
